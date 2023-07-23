160247f4f58c32f7292532844e485d88644c5c08
preciousnjukia <preciousnjukia@gmail.com>
2023-07-23 17:02:01 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function superbowlWin (record) {
+    const winningRecord = record.find(item => item.result === 'W');
+    return winningRecord;
+}
+
+function superbowlWin (record) {
+    const winningRecord = record.find(item => item.result === 'W');
+    return winningRecord ? winningRecord.year : undefined;
+}
